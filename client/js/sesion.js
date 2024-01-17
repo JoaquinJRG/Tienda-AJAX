959d0ff86bbf6dc33413a1ff0fc39040de935288
Joaquin <joaquinjrg01@gmail.com>
2024-01-17 13:17:40 +0100

    crear usuario

--- client/js/sesion.js
+++ client/js/sesion.js
@@ -1,10 +1,11 @@
 //Variables globales
 const loginDiv = document.getElementById("login");
+const loginForm = document.querySelector("#login form");
 const crearCuentaDiv = document.getElementById("crear-cuenta");
+const crearCuentaForm = document.querySelector("#crear-cuenta form");
 const principalDiv = document.getElementById("principal");
 
 //Funciones
-
 // Oculta el login y muestra el formulario para crear cuenta
 function mostrarCrearCuenta() {
     loginDiv.style.display = "none";
@@ -15,6 +16,48 @@ function mostrarCrearCuenta() {
 function mostrarLogin() {
     crearCuentaDiv.style.display = "none";
     loginDiv.style.display = "flex";
+}
+
+//Añade datos a BD y muestra mensaje
+function crearCuenta(event) {
+    //Evita que se recargue la página 
+    event.preventDefault(); 
+
+    let nombreUsuario = document.getElementById("crearUsuario");
+    let correoUsuario = document.getElementById("crearCorreo");
+    let claveUsuario = document.getElementById("crearClave");
+    let mensaje = document.querySelector("#crear-cuenta .mensaje");
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if(this.readyState == 4 && this.status == 200) {
+
+
+            if(this.responseText == "errorCampos") {
+                mensaje.innerHTML = "Error: Rellena los campos vacios";
+            } else if(this.responseText == "error") {
+                mensaje.innerHTML = "Error al crear la cuenta";
+            }else{
+                mensaje.innerHTML = "Cuenta creada correctamente";
+                nombreUsuario.value = "";
+                correoUsuario.value = "";
+                claveUsuario.value = "";
+            }
+            
+        }
+    };
+
+    xhttp.open("POST", "server/crear_cuenta.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(`nombreUsuario=${nombreUsuario.value}&correoUsuario=${correoUsuario.value}&claveUsuario=${claveUsuario.value}`);
+    return false;
+}
+
+//Inicia sesión o muestra mensaje en caso de error
+//Al iniciar sesión oculta el login y mustra la sección principal
+function iniciarSesion(e) {
+
 }
 
 
---
+++ server/crear_cuenta.php
@@ -0,0 +1,33 @@
+<?php
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $nombreUsuario = $_POST["nombreUsuario"];
+    $correoUsuario = $_POST["correoUsuario"];
+    $claveUsuario = $_POST["claveUsuario"]; 
+
+    //Todos los campos son obligatorios
+    if($nombreUsuario == "" || $correoUsuario == "" || $claveUsuario == "") {
+        echo "errorCampos"; 
+        return; 
+    }
+
+    require_once "conexion_bd.php";
+
+    $bd = conectarBD(); 
+
+    try {
+        $sql = "INSERT INTO usuario(nombre, correo, clave) 
+        VALUES('$nombreUsuario', '$correoUsuario', '$claveUsuario');";
+
+        $resultado = $bd->query($sql); 
+
+        echo "true";  
+
+    } catch (PDOException $e) {
+        echo "error"; 
+    }
+
+
+
+}
