816e4074fe9347c6503fb50060c625d83e36c41f
Joaquin <joaquinjrg01@gmail.com>
2024-01-19 13:22:11 +0100

    Estilos seccion categorias

--- client/js/cargarDatos.js
+++ client/js/cargarDatos.js
@@ -1,20 +1,31 @@
 //Variables globales
-const lista = document.getElementById("lista");
-
+const seccion = document.querySelector("section");
+const titulo = document.getElementById("titulo");
 
 //Funciones
+//Obtiene los datos de las categorías y representa los datos
 function cargarCategorias() {
 
-    lista.innerHTML = ""; 
+    seccion.innerHTML = ""; 
+    seccion.id = "categorias"
+    titulo.innerHTML = "Categorías";
     let xhttp = new XMLHttpRequest();
 
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let categorias = JSON.parse(this.responseText);
             categorias.forEach(cat => {
-                let li = document.createElement("li");
-                li.innerHTML = cat.nombre;
-                lista.appendChild(li);
+                let div = document.createElement("div");
+                let p = document.createElement("p");
+                let img = document.createElement("img"); 
+
+                img.src = `client/${cat.imagen}`;
+                div.classList.add("categoria");
+                p.innerHTML = cat.nombre;
+
+                div.appendChild(img);
+                div.appendChild(p);
+                seccion.appendChild(div);
             });
         }
     };
