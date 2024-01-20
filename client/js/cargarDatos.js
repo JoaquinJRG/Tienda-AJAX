b8e333241571731f06987bae08874914656be641
Joaquin <joaquinjrg01@gmail.com>
2024-01-20 20:41:14 +0100

    mostrar productos

--- client/js/cargarDatos.js
+++ client/js/cargarDatos.js
@@ -21,6 +21,11 @@ function cargarCategorias() {
 
                 img.src = `client/${cat.imagen}`;
                 div.classList.add("categoria");
+
+                div.onclick = function() {
+                    cargarProductos(cat.idCategoria, cat.nombre);
+                }
+
                 p.innerHTML = cat.nombre;
 
                 div.appendChild(img);
@@ -34,4 +39,40 @@ function cargarCategorias() {
     xhttp.send();
 
     return false;
+}
+
+//Obtiene los datos de los productos de una categoría determinada y los representa.
+function cargarProductos(idCategoria, nombreCategoria) {
+
+    seccion.innerHTML = ""; 
+    seccion.id = "productos";
+    titulo.innerHTML = nombreCategoria;
+
+    let xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            let productos = JSON.parse(this.responseText);
+            productos.forEach(prod => {
+                let div = document.createElement("div");
+                let p = document.createElement("p");
+                let img = document.createElement("img"); 
+
+                img.src = `client/${prod.imagen}`;
+                div.classList.add("producto");
+                p.innerHTML = prod.nombre;
+
+                div.appendChild(img);
+                div.appendChild(p);
+                seccion.appendChild(div);
+            });
+        }
+    };
+
+    xhttp.open("POST", "server/productos_json.php", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send(`idCategoria=${idCategoria}`);
+
+    return false;
+
 }
---
+++ server/productos_json.php
@@ -0,0 +1,26 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $idCategoria = $_POST["idCategoria"];
+
+    require_once "sesiones.php";
+    require_once "conexion_bd.php";
+
+    if (!comprobarSesion()) return;
+
+    $bd = conectarBD();
+
+    $sql = "SELECT idProducto, nombre, precio, imagen, stock FROM producto 
+    WHERE idCategoria = $idCategoria AND stock > 0;"; 
+
+    $resultado = $bd->query($sql);
+
+    //Consulta vacia
+    if($resultado->rowCount() == 0 || !$resultado){
+        echo "error"; 
+    }
+
+    echo json_encode(iterator_to_array($resultado)); 
+
+}
