d84eda1bd6fdf099a76c0f7239963bb7d9f800df
Joaquin <joaquinjrg01@gmail.com>
2024-01-30 19:46:33 +0100

    empiezo con el admin

---
+++ client/js/admin.js
@@ -0,0 +1,22 @@
+const titulo = document.getElementById("titulo");
+const inicioSection = document.getElementById("inicio");
+const adminSection = document.getElementById()
+
+function mostrarInicio() {
+    inicioSection.style.display = "flex";
+    adminSection.style.display = "none";  
+}
+
+function mostrarAnadirProductos() {
+    adminSection.style.display = "flex";  
+}
+
+function mostrarAnadirCategorias() {
+    adminSection.style.display = "flex";  
+
+}
+
+function mostrarEliminarProductos() {
+    adminSection.style.display = "flex";  
+
+}
