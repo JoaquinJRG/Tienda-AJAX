45f21d73b86edb7ab66ad640bad2b1439b9d12fd
Joaquin <joaquinjrg01@gmail.com>
2024-01-31 19:07:29 +0100

    html y css admin

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,33 +1,35 @@
-let tituloAdmin = document.querySelector("#admin #titulo");
+const tituloAdmin = document.querySelector("#admin #titulo");
 const inicioSection = document.getElementById("inicio");
-const anadirProductos = document.getElementById("anadir-productos");
-const anadirCategorias = document.getElementById("anadir-categorias");
-const eliminarProductos = document.getElementById("eliminar-productos");
+const anadirProductosAdmin = document.getElementById("anadir-productos");
+const anadirCategoriasAdmin = document.getElementById("anadir-categorias");
+const eliminarProductosAdmin = document.getElementById("eliminar-productos");
+
+
 
 function mostrarInicio() {
     tituloAdmin.innerHTML = "Administrar";
     inicioSection.style.display = "flex";
-    anadirProductos.style.display = "none";  
-    anadirCategorias.style.display = "none";  
-    eliminarProductos.style.display = "none";  
+    anadirProductosAdmin.style.display = "none";  
+    anadirCategoriasAdmin.style.display = "none";  
+    eliminarProductosAdmin.style.display = "none";  
 }
 
 function mostrarAnadirProductos() {
     tituloAdmin.innerHTML = "Añadir Productos";
     inicioSection.style.display = "none";
-    anadirProductos.style.display = "flex";  
+    anadirProductosAdmin.style.display = "flex";  
 }
 
 function mostrarAnadirCategorias() {
     tituloAdmin.innerHTML = "Añadir Categorías";
     inicioSection.style.display = "none";
-    anadirCategorias.style.display = "flex";  
+    anadirCategoriasAdmin.style.display = "flex";  
 
 }
 
 function mostrarEliminarProductos() {
     tituloAdmin.innerHTML = "Eliminar Productos";
     inicioSection.style.display = "none";
-    eliminarProductos.style.display = "flex";  
-
+    eliminarProductosAdmin.style.display = "flex";  
 }
+
